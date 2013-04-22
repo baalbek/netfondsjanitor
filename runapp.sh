c7524bd790c911bd0b0c1f282bdbd225d3a5f69a
Rolf C Stadheim <rolf.c.stadheim@gmail.com>
2013-04-22 22:45:05 +0200

    runapp.sh independent from clj

--- runapp.sh
+++ runapp.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
 # JAVA
-
 JAVA_HOME=/usr/local/java
 export JAVA_HOME
 
 export PATH=$JAVA_HOME/bin:$PATH
 
-
 cd /home/rcs/opt/java/netfondsjanitor/target
 
-/usr/local/bin/clj netfondsjanitor/app.clj
+CP=.:src:classes:test-classes
 
+for f in *.jar
+do
+    CP=$CP:$f
+done
+
+# /usr/local/bin/clj netfondsjanitor/app.clj
+
+exec java -cp $CP clojure.main netfondsjanitor/app.clj
 
 exit 0
 
