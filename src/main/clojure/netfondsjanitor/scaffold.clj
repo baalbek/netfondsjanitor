2692e8b9cabe522e69baa41d77e0e40bbd2fea6b
Rolf C Stadheim <rolf.c.stadheim@gmail.com>
2014-03-30 15:42:52 +0200

    More testing

--- src/main/clojure/netfondsjanitor/scaffold.clj
+++ src/main/clojure/netfondsjanitor/scaffold.clj
@@ -2,17 +2,21 @@
   (:import
     [org.joda.time LocalTime]
     [org.springframework.context.support ClassPathXmlApplicationContext]
-    [oahu.financial Etrade]
+    [oahu.financial Etrade StockPrice]
     [oahu.financial.html EtradeDownloader]
     [ranoraraku.models.mybatis StockMapper DerivativeMapper]
     [ranoraraku.beans StockPriceBean DerivativeBean]
     [java.io FileNotFoundException])
   (:require
-      [net.cgrand.enlive-html :as html]
-      [netfondsjanitor.service.common :as COM]
-      [netfondsjanitor.service.logservice :as LOG]
-      [netfondsjanitor.service.db :as DB]
-      [netfondsjanitor.service.feed :as FEED]))
+    [clojure.java.io :as io]
+    [net.cgrand.enlive-html :as html]
+    [netfondsjanitor.service.common :as COM]
+    [netfondsjanitor.service.logservice :as LOG]
+    [netfondsjanitor.service.db :as DB]
+    [netfondsjanitor.service.feed :as FEED])
+  (:use
+    [clojure.string :only [split join]]))
+
 
 
 (def ^:dynamic *base-url* "http://www.vg.no") ;"https://news.ycombinator.com/")
@@ -34,15 +38,18 @@
 (defn dl []
   (.getBean (factory) "downloader"))
 
-(def html-content
-  (memoize
-    (fn [ticker]
-      (let [page (.downloadDerivatives (dl) ticker)]
-        (-> page .getWebResponse .getContentAsString)))))
+(def cache (atom {}))
 
+(defn invalidate [ticker]
+  (reset! cache (dissoc @cache ticker)))
 
-(defn my-fetch []
-  (html/select (html-content "NHY") [:td]))
+(defn snip-ticker [ticker]
+  (if-let [result (@cache ticker)]
+    result
+    (let [page (.downloadDerivatives (dl) ticker)
+          new-result (html/html-snippet (-> page .getWebResponse .getContentAsString))]
+      (reset! cache (assoc @cache ticker new-result))
+      new-result)))
 
 (def hsel html/select)
 
@@ -50,6 +57,67 @@
 
 (def snip html/html-snippet)
 
-(defn opx [ticker]
-  (let [mysnip (html/html-snippet (html-content ticker))]
-    (:content (first (html/select mysnip [:table [:.com :.topmargin]])))))
+(defn stock-snip [ticker]
+  (let [mysnip (snip-ticker ticker)]
+    (remove #(= % "\n")
+      (:content (last (html/select mysnip [[:table#updatetable1] [:tr]]))))))
+
+(defn stock-vol [x]
+  (let [vol-str (first (:content (nth x 9)))]
+    (read-string (join (split vol-str #"\D")))))
+
+(defn spot [x]
+  (read-string (first (:content (nth x 0)))))
+
+(defn stock-opn [x]
+  (read-string (first (:content (nth x 5)))))
+
+(defn stock-hi [x]
+  (read-string (first (:content (nth x 6)))))
+
+(defn stock-lo [x]
+  (read-string (first (:content (nth x 7)))))
+
+(defn stock-buy [x]
+  (read-string (first (:content (first (:content (nth x 3)))))))
+
+(defn stock-sell [x]
+  (read-string (first (:content (first (:content (nth x 3)))))))
+
+(defn opx-snips [ticker]
+  (let [mysnip (snip-ticker ticker)
+        rows (:content (first (html/select mysnip [:table [:.com :.topmargin]])))
+        content-rows (map :content rows)
+        rows-ex-nil (remove #(nil? %) content-rows)
+        rm-eol (fn [v] (remove #(= % "\n") v))
+        rows-ex-eol (map rm-eol rows-ex-nil)]
+    (filter #(= (count %) 9) rows-ex-eol)))
+
+
+(defn titem []
+  (nth (opx "YAR") 18))
+
+(defn output [fname ticker]
+  (with-open [w (io/writer fname)]
+    (let [content (opx ticker)]
+      (doseq [line content]
+        (.write w (str line))))))
+    
+(defn opx-name [o]
+  (first (:content (first (:content (nth o 0))))))
+
+(defn opx-type [o]
+  (first (:content (nth o 1))))
+
+(defn opx-x [o]
+  (read-string (first (:content (nth o 2)))))
+
+(defn opx-exp [o]
+  (first (:content (nth o 3))))
+
+(defn opx-buy [o]
+  (read-string (first (:content (nth o 4)))))
+
+(defn opx-sell [o]
+  (read-string (first (:content (nth o 5)))))
+
