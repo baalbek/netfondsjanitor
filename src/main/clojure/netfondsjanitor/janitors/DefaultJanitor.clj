c11329efc11c6d3bacdbce0f71df06b88f411f9e
rcs <rolf.c.stadheim@gmail.com>
2014-12-03 22:50:20 +0100

    Refactoring do-harvest-files-with two overloads

--- src/main/clojure/netfondsjanitor/janitors/DefaultJanitor.clj
+++ src/main/clojure/netfondsjanitor/janitors/DefaultJanitor.clj
@@ -159,13 +159,6 @@
           (.insertStockPrice it s)
           )))))
 
-(defn do-upd-derivatives [^EtradeDerivatives etrade]
-  (let [tix-s (or *user-tix* (db-tix tcat-in-1-3))] ;#(= 1 (.getTickerCategory %))))]
-    (doseq [t tix-s]
-      (LOG/info (str "Will update derivatives for " t))
-      (let [derivx (.getCallPutDefs etrade t)]
-        (DB/insert-derivatives derivx)))))
-
 (comment
   (let [calls (.getCalls etrade t)
         puts (.getPuts etrade t)]
@@ -193,96 +186,104 @@
 (defmacro in? [v items]
   `(some #(= ~v %) ~items))
 
+
 (defn flatten-1
   [x]
   (filter #(and (sequential? %) (not-any? sequential? %))
     (rest (tree-seq #(and (sequential? %) (some sequential? %)) seq x))))
 
-(defn do-harvest-files-with [on-process-file
-                       ^EtradeDerivatives etrade
-                       tix
-                       from-date
-                       & [to-date]]
-  (let [to-datex (if (nil? to-date) from-date to-date)
-        tix-re (re-pattern "(\\S*)\\.html$")
-        short-months [4 6 9 11]
-        correct-date?
-          (fn [d m]
-            (cond
-              (in? m short-months) (<= d 30)
-              (= m 2) (<= d 28)
-              :else true))
-        all-days
-          (fn [y]
-            (fn [m]
-              (for [dx (range 1 32) :when (correct-date? dx m)]
-                [y m dx])))
-        full-year
-          (fn [y]
-            (for [mx (range 1 13) dx (range 1 32) :when (correct-date? dx mx)] [y mx dx]))
-        pm_
-          (fn [range-fn rr y m d]
-            (for [dx (range-fn d rr) :when (correct-date? dx m)]
-              [y m dx]))
-        month-end (partial pm_ drop (range 32))
-        month-begin (partial pm_ take (range 1 32))
-        year-end
-          (fn [y m d]
-            (let [a (month-end y m d)
-                  b (for [mx (range (+ m 1) 13)
-                          dx (range 1 32) :when (correct-date? dx mx)]
-                      [y mx dx])]
-              (concat a b)))
-        year-begin
-          (fn [y m d]
-            (let [a (for [mx (range 1 m)
-                          dx (range 1 32) :when (correct-date? dx mx)]
-                      [y mx dx])
-                  b (month-begin y m d)]
-              (concat a b)))
-        process-file
-          (fn [^File f]
-            (LOG/info (str "(Harvest) Trying file: " (.getPath f)))
-            (domonad maybe-m
-              [
-                cur-tix (re-matches tix-re (.getName f))
-                hit (in? (second cur-tix) tix)
-                scp (.getSpotCallsPuts2 etrade ^File f)
-                spot (.first scp)
-                calls (.second scp)
-                puts (.third scp)
-              ]
-              (on-process-file f spot calls puts)
-              ))
+(defn do-harvest-files-with 
+  ([on-process-file
+    ^EtradeDerivatives etrade
+    ctx]
+    (let [tix (or *user-tix* (db-tix tcat-in-1-3))
+          from-date (.ivHarvestFrom ctx) 
+          to-date (.ivHarvestTo ctx)]
+      (do-harvest-files-with on-process-file etrade from-date to-date)))
+  ([on-process-file
+    ^EtradeDerivatives etrade
+    tix
+    from-date
+    & [to-date]]
+    (let [to-datex (if (nil? to-date) from-date to-date)
+          tix-re (re-pattern "(\\S*)\\.html$")
+          short-months [4 6 9 11]
+          correct-date?
+            (fn [d m]
+              (cond
+                (in? m short-months) (<= d 30)
+                (= m 2) (<= d 28)
+                :else true))
+          all-days
+            (fn [y]
+              (fn [m]
+                (for [dx (range 1 32) :when (correct-date? dx m)]
+                  [y m dx])))
+          full-year
+            (fn [y]
+              (for [mx (range 1 13) dx (range 1 32) :when (correct-date? dx mx)] [y mx dx]))
+          pm_
+            (fn [range-fn rr y m d]
+              (for [dx (range-fn d rr) :when (correct-date? dx m)]
+                [y m dx]))
+          month-end (partial pm_ drop (range 32))
+          month-begin (partial pm_ take (range 1 32))
+          year-end
+            (fn [y m d]
+              (let [a (month-end y m d)
+                    b (for [mx (range (+ m 1) 13)
+                            dx (range 1 32) :when (correct-date? dx mx)]
+                        [y mx dx])]
+                (concat a b)))
+          year-begin
+            (fn [y m d]
+              (let [a (for [mx (range 1 m)
+                            dx (range 1 32) :when (correct-date? dx mx)]
+                        [y mx dx])
+                    b (month-begin y m d)]
+                (concat a b)))
+          process-file
+            (fn [^File f]
+              (LOG/info (str "(Harvest) Trying file: " (.getPath f)))
+              (domonad maybe-m
+                [
+                  cur-tix (re-matches tix-re (.getName f))
+                  hit (in? (second cur-tix) tix)
+                  scp (.getSpotCallsPuts2 etrade ^File f)
+                  spot (.first scp)
+                  calls (.second scp)
+                  puts (.third scp)
+                ]
+                (on-process-file f spot calls puts)
+                ))
 
-        process-dir
-          (fn [[y m d]]
-            (let [cur-dir (clojure.java.io/file (join "/" ["/home/rcs/opt/java/netfondsjanitor/feed" y m d]))
-                  files (filter #(.isFile %) (file-seq cur-dir))]
-              (doseq [cur-file files] (process-file cur-file))))
-        pfn
-          (fn [v]
-            (map read-string (split v #"-")))
-        [y1 m1 d1] (pfn from-date)
-        [y2 m2 d2] (pfn to-datex)]
-    (let [items (cond
-                  (and (= y1 y2) (= m1 m2) (= d1 d2))
-                    [[y1 m1 d1]]
-                  (= y1 y2)
-                    (let [months (drop 1 (range m1 m2))
-                          a (month-end y1 m1 d1)
-                          b (flatten-1 (map (all-days y1) months))
-                          c (month-begin y2 m2 d2)]
-                      (concat a b c))
-                  :else
-                    (let [years (drop 1 (range y1 y2))
-                          a (year-end y1 m1 d1)
-                          b (flatten-1 (map full-year years))
-                          c (year-begin y2 m2 d2)]
-                      (concat a b c)))]
-      (LOG/info (str "(Harvest) Processing files from: " from-date " to: " to-datex))
-      (doseq [cur-dir items] (process-dir cur-dir)))))
-
+          process-dir
+            (fn [[y m d]]
+              (let [cur-dir (clojure.java.io/file (join "/" ["/home/rcs/opt/java/netfondsjanitor/feed" y m d]))
+                    files (filter #(.isFile %) (file-seq cur-dir))]
+                (doseq [cur-file files] (process-file cur-file))))
+          pfn
+            (fn [v]
+              (map read-string (split v #"-")))
+          [y1 m1 d1] (pfn from-date)
+          [y2 m2 d2] (pfn to-datex)]
+      (let [items (cond
+                    (and (= y1 y2) (= m1 m2) (= d1 d2))
+                      [[y1 m1 d1]]
+                    (= y1 y2)
+                      (let [months (drop 1 (range m1 m2))
+                            a (month-end y1 m1 d1)
+                            b (flatten-1 (map (all-days y1) months))
+                            c (month-begin y2 m2 d2)]
+                        (concat a b c))
+                    :else
+                      (let [years (drop 1 (range y1 y2))
+                            a (year-end y1 m1 d1)
+                            b (flatten-1 (map full-year years))
+                            c (year-begin y2 m2 d2)]
+                        (concat a b c)))]
+        (LOG/info (str "(Harvest) Processing files from: " from-date " to: " to-datex))
+        (doseq [cur-dir items] (process-dir cur-dir))))))
 
 (defn iv-harvest [^File f,
                   ^StockPrice spot,
@@ -300,6 +301,20 @@
           (println (str "Option id: " (.getDerivativeId c))))))
     ;(.insertDerivativePrice it c))))
     (catch Exception e (LOG/error (str "[" (.getPath f) "] "(.getMessage e))))))
+
+(defn harvest-derivatives [^File f,
+                           ^StockPrice spot,
+                           calls,
+                           puts]
+  )
+
+(comment do-upd-derivatives [^EtradeDerivatives etrade]
+  (let [tix-s (or *user-tix* (db-tix tcat-in-1-3))] ;#(= 1 (.getTickerCategory %))))]
+    (doseq [t tix-s]
+      (LOG/info (str "Will update derivatives for " t))
+      (let [derivx (.getCallPutDefs etrade t)]
+        (DB/insert-derivatives derivx)))))
+
 ;;;------------------------------------------------------------------------
 ;;;-------------------------- Interface methods ---------------------------
 ;;;------------------------------------------------------------------------
@@ -313,9 +328,12 @@
       (doif .isFeed ctx (do-feed))
       (doif .isSpot ctx (do-spot (@s :etrade)))
       (doif .isIvHarvest ctx
-        (let [opx-tix (or *user-tix* (db-tix tcat-in-1-3))]
-          (do-harvest-files-with iv-harvest (@s :etrade) opx-tix (.ivHarvestFrom ctx) (.ivHarvestTo ctx))))
-      (doif .isUpdateDbOptions ctx (do-upd-derivatives (@s :etrade)))
+        (do-harvest-files-with iv-harvest (@s :etrade) ctx))
+        ;(let [opx-tix (or *user-tix* (db-tix tcat-in-1-3))]
+        ;  (do-harvest-files-with iv-harvest (@s :etrade) opx-tix (.ivHarvestFrom ctx) (.ivHarvestTo ctx))))
+      ;(doif .isUpdateDbOptions ctx (do-upd-derivatives (@s :etrade)))
+      (doif .isUpdateDbOptions ctx
+        (do-harvest-files-with harvest-derivatives (@s :etrade) ctx))
       (doif .isOneTimeDownloadOptions ctx
         (let [dl (@s :downloader)
               opx-tix (or *user-tix* (db-tix tcat-in-1-3))]
