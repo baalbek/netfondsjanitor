1b58cd06ca84d6fb064b0b09da5894be6aa02c87
rcs <rolf.c.stadheim@gmail.com>
2017-02-11 14:41:10 +0100

    chartWindow 100%

--- project.clj
+++ project.clj
@@ -4,16 +4,13 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
-		[org.aspectj/aspectjrt "1.8.9"]
-		[org.aspectj/aspectjtools "1.8.9"]
-		[org.aspectj/aspectjweaver "1.8.9"]
 		[org.springframework/spring-core "4.2.3.RELEASE"]
 		[org.springframework/spring-context "4.2.3.RELEASE"]
 		[org.springframework/spring-aop "4.2.3.RELEASE"]
 		[org.clojure/clojure "1.7.0"]
 		[org.clojure/clojure-contrib "1.2.0"]
 		[org.mybatis/mybatis "3.4.1"]
-		[junit/junit "4.11"]
+		[junit/junit "4.12"]
 		[log4j/log4j "1.2.17"]
 		[args4j/args4j "2.33"]
 		[org.clojure/algo.monads "0.1.5"]
@@ -21,11 +18,12 @@
 		[org.jsoup/jsoup "1.8.3"]
 		[org.ccil.cowan.tagsoup/tagsoup "1.2.1"]
 		[colt/colt "1.2.0"]
+		[org.aspectj/aspectjrt "1.8.9"]
 
     ]
   ;:main ^:skip-aot harborview.webapp
   ;:compile 
-  :target-path "build"
+  :target-path "target"
   :source-paths ["src/clojure"]
   :test-paths ["test/clojure" "dist" "test/resources"]
   :java-source-paths ["src/java" "test/java"]
